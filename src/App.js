dbdaa33a093515bbdc5decf0e7e3d8a83154de6c
Ozan Emre Dikbaş <87207946+OzanEmreDikbas@users.noreply.github.com>
2021-12-21 11:28:34 +0300

    2. commit.

--- src/App.js
+++ src/App.js
@@ -20,13 +20,31 @@ function App() {
   
   if (loading) {
     return (
-      <section className='section loadin'>
+      <section className='section loading'>
         <h1>loading...</h1>
       </section>
     );
   }
 
-  return <h2>jobs</h2>
+  const {company, dates, duties, title} = jobs[value];
+  return (
+    <section className='section'>
+      <div className='title'>
+        <h2>exprience</h2>
+      </div>         
+      <div className='jobs-center'>
+        {/*btn container */}
+        {/*job info*/}
+        <article className='job-info'>
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className='job-date'>{dates}</p>
+        </article>
+      </div>
+    </section>
+  
+  )
+
 }
 
 export default App;
